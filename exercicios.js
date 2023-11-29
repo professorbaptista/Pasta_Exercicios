180f8073d658ddda375cabcf5ea01d39af3072e8
professorbaptista <josbaptista28@yahoo.com.br>
2023-11-29 02:32:33 +0000

    Foi adicionado o javascript

---
+++ exercicios.js
@@ -0,0 +1,51 @@
+
+
+var trabalhar = true
+    var chuva = false
+    var res = document.getElementById('resp')
+    if (chuva == true){
+    
+    res.innerHTML = ('Não vou trabalhar') 
+    } 
+    else {
+        res.innerHTML = ('Vou trabalhar')
+        
+    }
+
+    var numeroDeAtraso = 2
+    if(numeroDeAtraso > 3){
+        document.getElementById('atraso').innerHTML = 'Introduz um número igual ou inferior à três.'
+    }
+    
+    else if(numeroDeAtraso == 3) {
+        document.getElementById('atraso').innerHTML = 'Não, não. Está suspenso!'
+    }
+    else if(numeroDeAtraso == 2){
+        document.getElementById('atraso').innerHTML = 'Pode entrar, mas se tomas mais uma falta, será suspenso.'
+    }
+    else if(numeroDeAtraso == 1){
+        document.getElementById('atraso').innerHTML = 'Pode entrar, mas tenha cuidado para não tomar a segunda falta.'
+    }
+    else{
+        document.getElementById('atraso').innerHTML = 'Parábens! Tu não tens nenhuma falta. Podes entrar.'
+    }
+
+    function calcular(){
+    var primeiroNumero = document.getElementById('num1')
+    var segundoNumero = document.getElementById('num2')
+    var resNum = document.getElementById('n1')
+    var n01 = Number(primeiroNumero.value)
+    var n02 = Number(segundoNumero.value)
+    if (n01 == '' || n02 == ''){
+        resNum.innerHTML = 'introduz um número!'
+    } 
+    else if (n01 > n02){
+        resNum.innerHTML = 'O primeiro numero é maior.'
+    }
+    else {
+        resNum.innerHTML = 'o segundo numero é maior.'
+    }
+
+    }
+
+    
